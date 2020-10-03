0a338cf56879700e0acdc191ec24602bbb47e399
Timo Reitz <godsboss@gmx.de>
2020-10-03 02:42:27 +0200

    Load image for gfx.

--- game/main.go
+++ game/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/GodsBoss/exit-condition-zero/pkg/game"
+	"github.com/GodsBoss/exit-condition-zero/pkg/imageload"
 	"github.com/GodsBoss/ld46/pkg/console"
 
+	"github.com/GodsBoss/gggg/pkg/dom"
 	"github.com/GodsBoss/gggg/pkg/dominit"
 )
 
@@ -14,6 +16,19 @@ func main() {
 }
 
 func run() error {
+	win, err := dom.GlobalWindow()
+	if err != nil {
+		return err
+	}
+	doc, err := win.Document()
+	if err != nil {
+		return err
+	}
+	loader := imageload.NewLoader(doc)
+	_, err = loader.Load("gfx.png")
+	if err != nil {
+		return err
+	}
 	dominit.Run(game.New())
 	<-make(chan struct{}, 0)
 	return nil
