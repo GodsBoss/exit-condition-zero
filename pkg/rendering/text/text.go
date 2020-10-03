5d222e9dea4d7e5b792ed78162a3b0069481e324
Timo Reitz <godsboss@gmx.de>
2020-10-03 21:50:42 +0200

    Let text render a background, too.

--- pkg/rendering/text/text.go
+++ pkg/rendering/text/text.go
@@ -34,11 +34,17 @@ func New(
 
 func (txt *Text) Render(output *dom.Context2D) {
 	lines := strings.Split(txt.contents, "\n")
-	renderables := make(rendering.Renderables, 0)
+	chars := make(rendering.Renderables, 0)
+	width := 0
+	height := len(lines)
+
 	for row := range lines {
+		if len(lines[row]) > width {
+			width = len(lines[row])
+		}
 		for col := range lines[row] {
-			renderables = append(
-				renderables,
+			chars = append(
+				chars,
 				txt.spriteMap.Produce(
 					"char_"+string(lines[row][col]),
 					txt.x+col*6,
@@ -49,7 +55,95 @@ func (txt *Text) Render(output *dom.Context2D) {
 			)
 		}
 	}
-	renderables.Render(output)
+
+	backgroundTiles := make(rendering.Renderables, 0)
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			backgroundTiles = append(
+				backgroundTiles,
+				txt.spriteMap.Produce(
+					"text_border_center",
+					txt.x+col*6,
+					txt.y+row*6,
+					txt.scale,
+					0,
+				),
+			)
+		}
+	}
+	for row := 0; row < height; row++ {
+		backgroundTiles = append(
+			backgroundTiles,
+			txt.spriteMap.Produce(
+				"text_border_left",
+				txt.x-3,
+				txt.y+row*6,
+				txt.scale,
+				0,
+			),
+			txt.spriteMap.Produce(
+				"text_border_right",
+				txt.x+width*6,
+				txt.y+row*6,
+				txt.scale,
+				0,
+			),
+		)
+	}
+	for col := 0; col < width; col++ {
+		backgroundTiles = append(
+			backgroundTiles,
+			txt.spriteMap.Produce(
+				"text_border_top",
+				txt.x+col*6,
+				txt.y-3,
+				txt.scale,
+				0,
+			),
+			txt.spriteMap.Produce(
+				"text_border_bottom",
+				txt.x+col*6,
+				txt.y+height*6,
+				txt.scale,
+				0,
+			),
+		)
+	}
+
+	backgroundTiles = append(
+		backgroundTiles,
+		txt.spriteMap.Produce(
+			"text_border_left_top",
+			txt.x-3,
+			txt.y-3,
+			txt.scale,
+			0,
+		),
+		txt.spriteMap.Produce(
+			"text_border_left_bottom",
+			txt.x-3,
+			txt.y+height*6,
+			txt.scale,
+			0,
+		),
+		txt.spriteMap.Produce(
+			"text_border_right_top",
+			txt.x+width*6,
+			txt.y-3,
+			txt.scale,
+			0,
+		),
+		txt.spriteMap.Produce(
+			"text_border_right_bottom",
+			txt.x+width*6,
+			txt.y+height*6,
+			txt.scale,
+			0,
+		),
+	)
+
+	backgroundTiles.Render(output)
+	chars.Render(output)
 }
 
 const allowedChars = "\nabcdefghijklmnopqrstuvwxyz 01234567890.,:;!?()[]-=><_+'\"%|~#/\\"
