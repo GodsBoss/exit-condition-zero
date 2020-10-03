bbcc1ec89ffcbeb259a365bde831baff3c819444
Timo Reitz <godsboss@gmx.de>
2020-10-03 06:34:25 +0200

    Add sprite map infos for backgrounds.

--- game/sprite_source_map.go
+++ game/sprite_source_map.go
@@ -4,4 +4,29 @@ import (
 	"github.com/GodsBoss/exit-condition-zero/pkg/rendering/sprite"
 )
 
-var spriteSources = sprite.SourceMap{}
+var spriteSources = sprite.SourceMap{
+	"bg_title": {
+		X: 480,
+		Y: 0,
+		W: 320,
+		H: 240,
+	},
+	"bg_levelselect": {
+		X: 480,
+		Y: 240,
+		W: 320,
+		H: 240,
+	},
+	"bg_playing": {
+		X: 480,
+		Y: 480,
+		W: 320,
+		H: 240,
+	},
+	"bg_gameover": {
+		X: 480,
+		Y: 720,
+		W: 320,
+		H: 240,
+	},
+}
