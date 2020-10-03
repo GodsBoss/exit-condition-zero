6361f7530906accc80147146fc218c910693b2a9
Timo Reitz <godsboss@gmx.de>
2020-10-03 02:37:22 +0200

    Log errors that occur.

--- game/main.go
+++ game/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"github.com/GodsBoss/exit-condition-zero/pkg/game"
+	"github.com/GodsBoss/ld46/pkg/console"
 
 	"github.com/GodsBoss/gggg/pkg/dominit"
 )
 
 func main() {
+	if err := run(); err != nil {
+		console.Global().LogMessage("Could not run game: %s", err.Error())
+	}
+}
+
+func run() error {
 	dominit.Run(game.New())
 	<-make(chan struct{}, 0)
+	return nil
 }
