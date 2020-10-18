4deea405bca93c43ea1dc43d2aa64014b91565b5
Timo Reitz <godsboss@gmx.de>
2020-10-18 20:30:54 +0200

    Document game.Renderables.

--- pkg/game/renderables.go
+++ pkg/game/renderables.go
@@ -4,8 +4,10 @@ import (
 	"github.com/GodsBoss/gggg/pkg/dom"
 )
 
+// Renderables combines several Renderables into one.
 type Renderables []Renderable
 
+// Render renders all Renderables, in order.
 func (r Renderables) Render(output *dom.Context2D) {
 	for i := range r {
 		r[i].Render(output)
